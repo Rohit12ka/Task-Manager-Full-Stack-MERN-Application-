b6b81bba8104a9c3f5606c88e394b838020f5ed7
Rohit <rohitkumari27965@gmail.com>
2026-08-04 20:49:22 +0530

    Taskservice

---
+++ client/src/Services/taskservice.js
@@ -0,0 +1,39 @@
+// src/services/TaskService.jsx
+
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/tasks";
+
+const TaskService = {
+  // Get all tasks
+  getTasks: async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+  },
+
+  // Get task by ID
+  getTaskById: async (id) => {
+    const response = await axios.get(`${API_URL}/${id}`);
+    return response.data;
+  },
+
+  // Create new task
+  createTask: async (task) => {
+    const response = await axios.post(API_URL, task);
+    return response.data;
+  },
+
+  // Update task
+  updateTask: async (id, task) => {
+    const response = await axios.put(`${API_URL}/${id}`, task);
+    return response.data;
+  },
+
+  // Delete task
+  deleteTask: async (id) => {
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.data;
+  },
+};
+
+export default TaskService;
